dd3a29dc93c71aba6c5de9b4b9fce33da5795f08
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-03-29 00:20:42 +0600

    feat: add database config

---
+++ src/config/database.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig = (config: ConfigService): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: config.get('db.host'),
+  port: config.get('db.port'),
+  username: config.get('db.username'),
+  password: config.get('db.password'),
+  database: config.get('db.name'),
+  autoLoadEntities: true,
+  synchronize: config.get('nodeEnv') !== 'production',
+  logging: config.get('nodeEnv') === 'development',
+});
