7466e2204f88a155b1c61f18a9e0769e5410b618
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-03-29 09:43:38 +0600

    feat: bootstrap app with Helmet, CORS, Swagger, rate limiting

---
+++ src/app.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_GUARD, APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+
+import appConfig from './config/app.config';
+import { typeOrmConfig } from './config/database.config';
+import { winstonConfig } from './config/logger.config';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CartModule } from './modules/cart/cart.module';
+import { OrdersModule } from './modules/orders/orders.module';
+
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig] }),
+    winstonConfig,
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    CartModule,
+    OrdersModule,
+  ],
+  providers: [
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_FILTER, useClass: GlobalExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const cookieParser = require('cookie-parser');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('port')!;
+  const isDev = config.get('nodeEnv') !== 'production';
+
+  app.use(helmet());
+  app.enableCors({ origin: config.get('frontendUrl'), credentials: true });
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  if (isDev) {
+    const doc = new DocumentBuilder()
+      .setTitle('Mini E-Commerce API')
+      .setDescription('Production-grade NestJS + MySQL REST API')
+      .setVersion('1.0')
+      .addCookieAuth('access_token')
+      .build();
+    SwaggerModule.setup(
+      'api/docs',
+      app,
+      SwaggerModule.createDocument(app, doc),
+    );
+  }
+
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
+  if (isDev) console.log(`Swagger docs at http://localhost:${port}/api/docs`);
+}
+
+bootstrap();
