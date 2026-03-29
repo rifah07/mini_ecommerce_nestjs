bd016d3f66807e1bc559f88366ac7f4ee51f7ea5
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-03-29 10:49:54 +0600

    fix: update db configuration file for solving render problem

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,15 +3,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig = (config: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
-  host: config.get('db.host'),
-  port: config.get('db.port'),
-  username: config.get('db.username'),
-  password: config.get('db.password'),
-  database: config.get('db.name'),
+  host: config.get('DB_HOST'),
+  port: Number(config.get('DB_PORT')),
+  username: config.get('DB_USERNAME'),
+  password: config.get('DB_PASSWORD'),
+  database: config.get('DB_NAME'),
   autoLoadEntities: true,
-  synchronize: config.get('nodeEnv') !== 'production',
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  logging: config.get('nodeEnv') === 'development',
+  synchronize: config.get('NODE_ENV') !== 'production',
+  ssl: { rejectUnauthorized: false },
+  logging: config.get('NODE_ENV') === 'development',
 });
